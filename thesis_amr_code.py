548f245ad557ad2f84cb4facafa9a9dd31b02eb6
Prem Kumar Loganathan <premkumar.loganathan@student.kuleuven.be>
2026-06-11 17:53:39 +0200

    Add files via upload

--- thesis_amr_code.py
+++ thesis_amr_code.py
@@ -70,41 +70,74 @@ ws = full_ws['Mean_Wind'][2:]
 
 # %% AMR Data preprocessing pt.1
 
-columns_of_interest = ['labIsolCode', 'repCountry', 'matrix', 'sampY', 'sampM', 'Active Substance', 'MIC', 'cutoffValue']
+columns_of_interest = ['labIsolCode', 'repCountry', 'matrix', 'sampY', 'sampM', 'sampD', 'Active Substance', 'MIC', 'cutoffValue']
 amr_data = amr_df[columns_of_interest]
 
 matrix_map = {'PRI 035': 'Pigs', 'PRI 036': 'Calves', 'PRI 019 Broilers': 'Poultry', 'PRI 019 Turkeys': 'Poultry'}
 amr_data['Animal Type'] = amr_data['matrix'].map(matrix_map)
-amr_data['Positive'] = (amr_data['cutoffValue'] > amr_data['MIC']).astype(int)
+amr_data['Positive'] = (amr_data['MIC'] > amr_data['cutoffValue']).astype(int)
 amr_data['YY-MM'] = amr_data['sampY'].astype(str) + '-' + amr_data['sampM'].astype(str)
 amr_data['YY-MM'] = pd.to_datetime(amr_data['YY-MM'], format = '%Y-%m')
-
 period_range = pd.date_range(start = amr_data['YY-MM'].min(),
                              end = amr_data['YY-MM'].max(), freq = 'MS')
-amr_data_grouped = amr_data.groupby(['YY-MM', 'Animal Type', 'Active Substance', 'labIsolCode'])[['Positive']].sum().reset_index()
+counts = amr_data.groupby(['YY-MM', 'Animal Type', 'Active Substance'])[['labIsolCode']].nunique().reset_index()
+resistant_counts = amr_data.groupby(['YY-MM', 'Animal Type', 'Active Substance'])[['Positive']].sum().reset_index()
+
+full_data = resistant_counts.copy()
+full_data = full_data.drop(columns = 'Positive')
+full_data['Resistant'] = resistant_counts['Positive'] / counts['labIsolCode']
+
+amr_table = pd.pivot_table(data = full_data, columns = ['Animal Type', 'Active Substance'], index = 'YY-MM', values = 'Resistant')
+amr_table
+
+'''
 amr_data_grouped['Counts'] = [1] * len(amr_data_grouped)
 amr_data_grouped_with_counts = amr_data_grouped.groupby(['YY-MM', 'Animal Type', 'Active Substance'])[['Counts', 'Positive']].sum().reset_index()
 
 amr_data_pigs = amr_data_grouped_with_counts[amr_data_grouped_with_counts['Animal Type'] == 'Pigs']
-amr_data_pigs_amp = amr_data_pigs[amr_data_pigs['Active Substance'] == 'Ampicillin']
+amr_data_pigs_amp = amr_data_pigs[amr_data_pigs['Active Substance'] == 'Gentamicin']
 
 amr_data_pigs_amp = (amr_data_pigs_amp.set_index('YY-MM').reindex(period_range).reset_index().rename(columns = {'index': 'YY-MM'}))
 
 fig, ax = plt.subplots(figsize = (8, 6))
-ax.plot(amr_data_pigs_amp['YY-MM'], amr_data_pigs_amp['Counts'], 'o-', color = 'grey')
-ax.plot(amr_data_pigs_amp['YY-MM'], (amr_data_pigs_amp['Positive']/amr_data_pigs_amp['Counts']) * 100, 'o-', color = 'lightskyblue')
+ax.plot(amr_data_pigs_amp['YY-MM'], amr_data_pigs_amp['Counts'], 'o-', color = 'grey', label = 'Tested')
+ax.plot(amr_data_pigs_amp['YY-MM'], (amr_data_pigs_amp['Positive']/amr_data_pigs_amp['Counts']) * 100, 'o-', color = 'lightskyblue', label = 'Resistant')
+ax.legend()
+ax.set_xlabel('Time[Months]')
+ax.set_ylabel('No of tested units \n& Resistance (%)')
+ax.set_title('Ampicillin Resistant E. coli occurance in Pigs', loc = 'left')
+plt.tight_layout()
+plt.show()
 
 amr_data_pigs_amp_q = (amr_data_pigs_amp.set_index('YY-MM')
     .resample('QE')[['Counts', 'Positive']].sum().reset_index())
 fig, ax = plt.subplots(figsize = (8, 6))
-ax.plot(amr_data_pigs_amp_q['YY-MM'], amr_data_pigs_amp_q['Counts'], 'o-', color = 'grey')
-ax.plot(amr_data_pigs_amp_q['YY-MM'], (amr_data_pigs_amp_q['Positive']/amr_data_pigs_amp_q['Counts']) * 100, 'o-', color = 'lightskyblue')
+ax.plot(amr_data_pigs_amp_q['YY-MM'], amr_data_pigs_amp_q['Counts'], 'o-', color = 'grey', label = 'Tested')
+ax.plot(amr_data_pigs_amp_q['YY-MM'], (amr_data_pigs_amp_q['Positive']/amr_data_pigs_amp_q['Counts']) * 100, 'o-', color = 'lightskyblue', label = 'Resistant')
+ax.legend()
+ax.set_xlabel('Time[Quarters]')
+ax.set_ylabel('No of tested units \n& Resistance (%)')
+ax.set_title('Ampicillin Resistant E. coli occurance in Pigs', loc = 'left')
+plt.tight_layout()
+plt.show()
+
+amr_data_pigs_amp_y = (amr_data_pigs_amp.set_index('YY-MM')
+    .resample('YS')[['Counts', 'Positive']].sum().reset_index())
+fig, ax = plt.subplots(figsize = (8, 6))
+# ax.plot(amr_data_pigs_amp_y['YY-MM'], amr_data_pigs_amp_y['Counts'], 'o-', color = 'grey', label = 'Tested')
+ax.plot(amr_data_pigs_amp_y['YY-MM'], (amr_data_pigs_amp_y['Positive']/amr_data_pigs_amp_y['Counts']) * 100, 'o-', color = 'lightskyblue', label = 'Resistant')
+ax.legend()
+ax.set_xlabel('Time[Years]')
+ax.set_ylabel('No of tested units \n& Resistance (%)')
+ax.set_title('Ampicillin Resistant E. coli occurance in Pigs', loc = 'left')
+plt.tight_layout()
+plt.show()
 
 amr_data_grouped_with_counts['Resistance'] = (amr_data_grouped_with_counts['Positive'] / amr_data_grouped_with_counts['Counts']) * 100
 
 table_for_grouped = pd.pivot_table(data = amr_data_grouped_with_counts, index = 'YY-MM', columns = ['Animal Type', 'Active Substance'], values = 'Resistance')
 table_for_grouped = (table_for_grouped.reindex(period_range).reset_index().rename(columns = {'index': 'YY-MM'}))
-
+'''
 def has_consecutive_nans(series, threshold): # Thanks to GenAI
     
     nan_vals = series.isna()
@@ -113,7 +146,23 @@ def has_consecutive_nans(series, threshold): # Thanks to GenAI
     
     return (streaks >= threshold).any()
 
-cols_to_drop = [col for col in table_for_grouped.columns if has_consecutive_nans(table_for_grouped[col], 5)]
-amr_df_cleaned = table_for_grouped.drop(columns = cols_to_drop)
-loss = ((table_for_grouped.shape[1] - amr_df_cleaned.shape[1]) / table_for_grouped.shape[1]) * 100
+cols_to_drop = [col for col in res_table.columns if has_consecutive_nans(res_table[col], 5)]
+amr_df_cleaned = res_table.drop(columns = cols_to_drop)
+loss = ((res_table.shape[1] - amr_df_cleaned.shape[1]) / res_table.shape[1]) * 100
 print(f'Loss: {np.round(loss, 2)}%')
+
+# amr_df_cleaned = amr_df_cleaned.set_index('YY-MM')
+# Plotting the figure to see if the inconsistency is continous
+
+fig, ax = plt.subplots(6, 7, figsize=(16, 12), sharex = True)
+ax = ax.flatten()
+
+for i, j in enumerate(amr_df_cleaned.columns):
+    ax[i].plot(amr_df_cleaned.index, amr_df_cleaned[j], 'o-', label = j, markersize = 2)
+    ax[i].set_title(j, loc = 'left', size = 8)
+    ax[i].grid('--', c = 'grey', alpha = 0.3)
+    ax[i].set_xticks(amr_df_cleaned.index[::12])
+    ax[i].tick_params('x', rotation = 90)
+
+ABs_with_100_and_0_resistance = ['Cefotaxime', 'Meropeneme', 'Ceftazidime', 'Colistin', 'Tigecycline'] 
+amr_df_cleaned_2 = amr_df_cleaned.loc[:, ~amr_df_cleaned.columns.get_level_values(1).isin(ABs_with_100_and_0_resistance)]
