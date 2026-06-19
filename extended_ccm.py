41502da321617e152f22ceebf8ce58e82234df5b
Prem Kumar Loganathan <premkumar.loganathan@student.kuleuven.be>
2026-06-19 16:18:03 +0200

    Add files via upload

--- extended_ccm.py
+++ extended_ccm.py
@@ -1,29 +1,21 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 
-def ccm_result_1(func, X, Y, L_range, tau_y, tau_x, E_y, E_x):
-    Xhat_My, Yhat_Mx = [], []
-    for L in tqdm(L_range): 
-        ccm_XY = func(X, Y, tau_y, E_y, L) # define new ccm object # Testing for X -> Y
-        ccm_YX = func(Y, X, tau_x, E_x, L) # define new ccm object # Testing for Y -> X    
+def extended_ccm(func, X, Y, L, tau_y, tau_x, E_y, E_x, tp, variable):
+    
+    ccm_XY = func(X, Y, tau_y, E_y, L) # Checking if X -> Y
+    ccm_YX = func(Y, X, tau_x, E_x, L) # Checking if Y -> X
+    rho_xy, rho_yx = [], []
+    for i in range(-tp, tp+1):
+        rho_xy.append(ccm_XY.causality(tp = i)[0][1])
+        rho_yx.append(ccm_YX.causality(tp = i)[0][1])
 
-        rho_xy = ccm_XY.causality()[0][1]
-        rho_yx = ccm_YX.causality()[0][1]
-
-        Xhat_My.append(rho_xy) 
-        Yhat_Mx.append(rho_yx) 
-
-    fig, ax = plt.subplots(1, 1, figsize=(5,5))
-    ax.plot(L_range, Xhat_My, label='AMR causes Precipitation', linewidth = 1.5)
-    ax.plot(L_range, Yhat_Mx, c='r', label='Precipitation causes AMR', linewidth = 1.5)
-    ax.set_xlabel('Time Series Length [L]',)
+    fig, ax = plt.subplots(1, 1, figsize = (6, 4))
+    ax.plot(np.arange(-tp, tp+1), rho_xy, 'o-', color = 'blue', label = f'AMR causes {variable}', lw = 1.2)
+    ax.plot(np.arange(-tp, tp+1), rho_yx, '^-', color = 'red', label = f'{variable} causes AMR', lw = 1.2)
+    ax.set_xlabel('Time Horizon (Tp)')
     ax.set_ylabel(r'$CCM\ Skill\ (\rho)$')
     ax.legend(loc = 'upper right')  
     ax.grid(color = 'grey', alpha = 0.3)
     ax.set_title('Convergent Cross Mapping Skill as \na Function of Library Size', loc = 'left')
     return ax
-
-def extended_ccm(func, X, Y, L, tau_y, tau_x, E_y, E_x, tp):
-    Xhat_My, Yhat_Mx = [], []
-    for T in np.arange(-tp, tp + 1, 1):
-        
